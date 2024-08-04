0be79b4146c046f63aabac16724083d3e44daa87
Mira Dalati <miradalati@gmail.com>
2024-08-04 13:57:42 +0300

    Adding new students

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.get('/', function (req, res) {
     res.send('Hello World')
   })
 
+  app.get('/students', function (req, res) {
+    var data = [
+        {id:1 , name: "Mira"},
+        {id:2 , name: "Reina"},
+        {id:3 , name: "Tala"},
+        {id:4 , name: "Lamar"}
+    ]
+    res.send(data)
+  })
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
